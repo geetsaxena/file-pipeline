c4b34c66bf4d09fbc7f038316b8929547b69ca11
Ashish <ashish.saxena25@gmail.com>
2020-11-23 12:42:19 -0500

    Runing program on EC2

--- src/main/java/HelloJavaWorld.java
+++ src/main/java/HelloJavaWorld.java
@@ -4,5 +4,9 @@ public class HelloJavaWorld {
         System.out.println("Hello world......");
         System.out.println("This is Geet");
         System.out.println("I am learning Java");
+        System.out.println(" I am runing this program on EC2 ");
+
     }
+
+
 }
